80e3a74ef7054b4e1bbfb9221a920980ade9f878
Zach Mohan <zmohan@wustl.edu>
2019-01-11 19:34:08 -0500

    Added g command and prefixes

--- ment.py
+++ ment.py
@@ -1,24 +1,25 @@
 import discord
-from discord.ext import commands
 from discord.ext.commands import Bot
 import os
 import asyncpg
 import ssl
-import secrets
 import json
-from datetime import datetime, timedelta
-from pytz import timezone
 from decimal import Decimal
-import asyncio
-import operator
 import time
+import requests
+# from discord.ext import commands
+# import secrets
+# from datetime import datetime, timedelta
+# from pytz import timezone
+# import asyncio
+# import operator
 
 
 TOKEN = os.environ['token']  # The token is also substituted for security reasons
 DATABASE_URL = os.environ['DATABASE_URL']
 
 
-bot = Bot(command_prefix=".", case_insensitive=True)
+bot = Bot(command_prefix=["=", "!", "-", "\\", ">", "+", "(", "#"], case_insensitive=True)
 client = discord.Client()
 
 
@@ -167,6 +168,92 @@ async def ping(ctx):
     embed = discord.Embed(color=0x0099cc)
     embed.add_field(name="Ping", value=f"💓Heartbeat: {heartbeat:,.2f}ms\t💬ACK: {millis:,.2f}ms.")
     await msg.edit(embed=embed)
+
+
+@bot.command()
+async def g(ctx, member: discord.Member, amount: int):
+    with open('keys.json') as json_file:
+        data = json.load(json_file)
+
+    for keys in data["keys"]:
+        if keys["prefix"] == ctx.prefix:
+            amount = amount * int(keys["value"])
+            currency = keys["currency"]
+    await update_money(member.id, amount, currency)
+    embed = discord.Embed(color=0x0099cc)
+    embed.set_author(name=member.display_name, icon_url=member.avatar_url)
+    embed.add_field(name=f"Wager Update", value=f"Successfully added {format_from_k(amount)} {currency} to "
+                                                f"{member.display_name}'s wager")
+    await ctx.send(embed=embed)
+
+
+def xp_99(current_xp: int):
+    return 0 if current_xp > 13_034_431 else "{:,}".format(13_034_431 - current_xp)
+
+
+# @bot.command()
+async def xp(ctx, *, username: str):
+    url = f'http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player={username}'
+    response = requests.get(url)
+    if response.status_code == requests.codes.not_found:
+        not_found = "**<:error:513794294763618305> Stats weren't found, the account might not exist / is banned.**"
+        await ctx.send(not_found)
+    embed = discord.Embed(description="**" + response.text + "**", color=0x00FF00)
+    embed.set_author(name=username,
+                     icon_url="https://imgb.apk.tools/115/b/c/2/com.jagex.oldscape.android.png")
+    embed.set_thumbnail(
+        url="https://vignette1.wikia.nocookie.net/ikov-2/images/2/25/Unnamed_%281%29.png/revision/"
+            "latest?cb=20170111043504")
+    stats = response.text.split(",")
+    # print(stats)
+    attxp = stats[4]
+    attxp1 = attxp.split("\n")
+    realattxp = attxp1[0]
+
+    del stats[0]
+    del stats[1]
+    del stats[2]
+    for i in range(0, int(len(stats) / 2)):
+        del stats[i]  # +1
+    del stats[23]
+    del stats[23]
+    del stats[23]
+    del stats[23]
+    del stats[23]
+    new_stats = response.text.split(",")
+    stats.insert(1, new_stats[3])
+    stats = [i.split("\n", 1)[0] for i in stats]
+
+    embed = discord.Embed(description=f"<:Attack:498591810210496515> {xp_99(int(realattxp))}"
+                                      f"<:Hitpoints:498591928938397707> {xp_99(int(stats[4]))}"
+                                      f"<:Mining:498591929433456642> {xp_99(int(stats[15]))}"
+                                      f"\n<:Strength:498592002288517121> {xp_99(int(stats[3]))}"
+                                      f"<:Agility:498591795006013471> {xp_99(int(stats[17]))}"
+                                      f"<:Smithing:498592002276065281> {xp_99(int(stats[14]))}"
+                                      f"\n<:Defence:498591848974254083> {xp_99(int(stats[2]))}"
+                                      f"<:Herblore:498591928816893954> {xp_99(int(stats[16]))}" 
+                                      f"<:Fishing:498591879852589056> {xp_99(int(stats[11]))}"
+                                      f"<:Ranged:498592002129133570> {xp_99(int(stats[5]))}"
+                                      f"<:Thieving:498592002435186688> {xp_99(int(stats[18]))}"
+                                      f"<:Cooking:498591829357494272> {xp_99(int(stats[8]))}"
+                                      f"<:Prayer:498592002276065280> {xp_99(int(stats[6]))}"
+                                      f"<:Crafting:498591838652071956> {xp_99(int(stats[13]))}"
+                                      f"<:Firemaking:498591866258980916> {xp_99(int(stats[12]))}"
+                                      f"\n<:Magic:498591928909299762> {xp_99(int(stats[7]))}"
+                                      f"<:Fletching:498591893513306112> {xp_99(int(stats[10]))}"
+                                      f"<:Woodcutting:498592002393243648> {xp_99(int(stats[9]))}"
+                                      f"\n<:Runecrafting:498592002259156993> {xp_99(int(stats[21]))}"
+                                      f"<:Slayer:498592001902510092> {xp_99(int(stats[19]))}"
+                                      f"<:Farming:498591858046271498> {xp_99(int(stats[20]))}"
+                                      f"<:Construction:498591820477890600> {xp_99(int(stats[23]))}" 
+                                      f"<:Hunter:498591928779145217> {xp_99(int(stats[22]))}"
+                                      f"<:Warding:498604407496376320>0", color=0xFF0000)
+    embed.set_author(name=username,
+                     icon_url="https://imgb.apk.tools/115/b/c/2/com.jagex.oldscape.android.png")
+    embed.set_thumbnail(
+        url="https://vignette1.wikia.nocookie.net/ikov-2/images/2/25/Unnamed_%281%29.png/revision/"
+            "latest?cb=20170111043504")
+    await ctx.send(embed=embed)
 
 
 bot.load_extension("libneko.extras.superuser")
